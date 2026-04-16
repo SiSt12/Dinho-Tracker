ef2c3676c4a671b933fb06846bf63975f8a95062
Silvaldo-Stolarz <silvaldostolarz@vibraenergia.com.br>
2026-04-16 15:50:51 -0300

    Delete all files to add th rebuilt from scratch new version of the app

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://iddnpicitcngcdjqtbki.supabase.co';
-const supabaseKey = 'sb_publishable_t_7K4FslEmnuUwlXV96RbQ_7G-gbFsN';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
